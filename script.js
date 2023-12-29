83464a835dd055c74a269f2ea16ade16a534ba06
sanjana <sanjana.bansal@loblaw.ca>
2023-12-29 17:14:08 -0500

    Revert "improved styles"
    This reverts commit 28e35e362376f64d4fa5a0c0e31a25ff8c4f65aa.

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
-$(document).ready(function () {
-  // Click event on candles
-  $(".candles").click(function (event) {
-    event.stopPropagation(); // Prevent the click event from reaching the document body
-    $(".flame").animate({ "opacity": 0 }, "fast");
-    $(".flame2").animate({ "opacity": 0 }, "fast");
-    $(".flame3").animate({ "opacity": 0 }, "fast");
-    $(".text").animate({ "top": -180, "opacity": 1 }, "fast");
-    // Push new confetti objects to `particles[]`
-    for (var i = 0; i < maxConfettis; i++) {
-      particles.push(new confettiParticle());
-    }
-    Draw();
-  });
-
-  // Click event on the document body (background)
-  $(document.body).click(function () {
-    $(".flame").animate({ "opacity": 1 }, "fast");
-    $(".flame2").animate({ "opacity": 1 }, "fast");
-    $(".flame3").animate({ "opacity": 1 }, "fast");
-    $(".text").animate({ "top": 0, "opacity": 0 }, "fast");
-    particles.length = 0;
-    context.clearRect(0, 0, W, window.innerHeight);
-  });
+$(document).ready(function() {
+    $(".candles").click(function() {
+        
+      $(".flame").animate({"opacity": 0}, "fast");
+      $(".flame2").animate({"opacity": 0}, "fast");
+      $(".flame3").animate({"opacity": 0}, "fast");
+      $(".text").animate({"top": -130, "opacity": 1}, "normal");
+      $("#confetti").show();
+      Draw();
+    });
 });
 
 let W = window.innerWidth;
@@ -56,7 +42,7 @@ function confettiParticle() {
   this.tiltAngleIncremental = Math.random() * 0.07 + 0.05;
   this.tiltAngle = 0;
 
-  this.draw = function () {
+  this.draw = function() {
     context.beginPath();
     context.lineWidth = this.r / 2;
     context.strokeStyle = this.color;
@@ -75,7 +61,7 @@ function Draw() {
   context.clearRect(0, 0, W, window.innerHeight);
 
   for (var i = 0; i < maxConfettis; i++) {
-    results.push(particles[i]?.draw());
+    results.push(particles[i].draw());
   }
 
   let particle = {};
@@ -83,20 +69,18 @@ function Draw() {
   for (var i = 0; i < maxConfettis; i++) {
     particle = particles[i];
 
-    if (particle) {
-      particle.tiltAngle += particle.tiltAngleIncremental;
-      particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2;
-      particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15;
+    particle.tiltAngle += particle.tiltAngleIncremental;
+    particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2;
+    particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15;
 
-      if (particle.y <= H) remainingFlakes++;
+    if (particle.y <= H) remainingFlakes++;
 
-      // If a confetti has fluttered out of view,
-      // bring it back to above the viewport and let if re-fall.
-      if (particle.x > W + 30 || particle.x < -30 || particle.y > H) {
-        particle.x = Math.random() * W;
-        particle.y = -30;
-        particle.tilt = Math.floor(Math.random() * 10) - 20;
-      }
+    // If a confetti has fluttered out of view,
+    // bring it back to above the viewport and let if re-fall.
+    if (particle.x > W + 30 || particle.x < -30 || particle.y > H) {
+      particle.x = Math.random() * W;
+      particle.y = -30;
+      particle.tilt = Math.floor(Math.random() * 10) - 20;
     }
   }
 
@@ -105,7 +89,7 @@ function Draw() {
 
 window.addEventListener(
   "resize",
-  function () {
+  function() {
     W = window.innerWidth;
     H = window.innerHeight;
     canvas.width = window.innerWidth;
@@ -113,6 +97,11 @@ window.addEventListener(
   },
   false
 );
+
+// Push new confetti objects to `particles[]`
+for (var i = 0; i < maxConfettis; i++) {
+  particles.push(new confettiParticle());
+}
 
 // Initialize
 canvas.width = W;
